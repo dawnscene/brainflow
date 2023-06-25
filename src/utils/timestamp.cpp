67578a884f6f23aba3b13cae8bd8f38fc5ee8b53
jchen@dawnscene <jidong.chen@dawnscene.com>
2023-06-26 00:11:03 +0800

    Use steady_clock instead of system_clock for timestamp

--- src/utils/timestamp.cpp
+++ src/utils/timestamp.cpp
@@ -1,14 +1,18 @@
-#ifdef _WIN32
-#include <stdint.h>
-#include <windows.h>
-#define FILETIME_TO_UNIX 116444736000000000i64
-#else
-#include <sys/time.h>
-#endif
-
 #include "timestamp.h"
-#include <stdlib.h>
+#include <chrono>
+#include <cstdlib>
 
+double get_timestamp() {
+    int64_t local_clock_ns = std::chrono::nanoseconds(std::chrono::steady_clock::now().time_since_epoch()).count();
+	const auto ns_per_s = 1000000000;
+	const auto seconds_since_epoch = std::lldiv(local_clock_ns, ns_per_s);
+	/* For large timestamps, converting to double and then dividing by 1e9 loses precision
+	   because double has only 53 bits of precision.
+	   So we calculate everything we can as integer and only cast to double at the end */
+	return seconds_since_epoch.quot + static_cast<double>(seconds_since_epoch.rem) / ns_per_s;
+}
+
+/*
 #ifdef _WIN32
 double get_timestamp ()
 {
@@ -25,3 +29,4 @@ double get_timestamp ()
     return (double)(tv.tv_sec) + (double)(tv.tv_usec) / 1000000.0;
 }
 #endif
+*/
